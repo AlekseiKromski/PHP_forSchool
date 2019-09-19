9d833d72f75b0ac0591f8e31546e3c8ba8313f88
pupil <pupil@ivkhk.ee>
2019-09-19 08:20:21 +0300

    9/19/2019

---
+++ 16/Kromski.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'functions.php';
+?>
+<html>
+    <head>
+        <title>Выполнение контрольной работы. Задание 1</title>
+        <link rel="stylesheet" href="css/style.css" type="text/css"/>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+    	<?php
+
+	    	//Массивы
+    		
+
+    	?>
+    </body>
+</html>
+
