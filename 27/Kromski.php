7a5504a95a6cf349ff5a2f970f1a608fd940e3fb
AlekseiKromski <Aleksei.Kromski@ivkhk.ee>
2019-10-14 10:22:39 +0300

    10/14/2019

--- 27/Kromski.php
+++ 27/Kromski.php
@@ -9,60 +9,80 @@ require_once 'functions.php'; //for debuging array
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
-    	<?php debug($_POST); ?>
-    <form method="post"> 
-    	<input type="text" name="name">
-	    <?php
-		    $file = file('Ques.txt');
-		        foreach ($file as $line) {
+	    <form method="post">
+	    	<input type="text" name="name">
+	    	<?php
+	    		$file_ques = file('Ques.txt');
+	    		foreach ($file_ques as $line) {
 		            $info = explode('|', $line);
-		            echo "<h5>{$info[0]}</h5>";
-		            echo "
-		            	{$info[1]}
-						<input type='radio' name='$info[0]' value='$info[1]'>
-		            ";
-		            echo "
-		            	{$info[2]}
-						<input type='radio' name='$info[0]' value='$info[2]'>
-		            ";
-		            echo "
-		            	{$info[3]}
-						<input type='radio' name='$info[0]' value='$info[3]'>
-		            ";
-		        }
-		    ?>   
-		<br><br>	
-		<input type="submit" value="ответить">
-		<?php
-		if($_SERVER['REQUEST_METHOD'] == 'POST'){
-			?>
-			<table class="table">
-			  <thead>
-			    <tr>
-			      <th scope="col">Вопросы</th>
-			      <th scope="col">Ваши ответы</th>
-			    </tr>
-			  </thead>
-			  <tbody>
-			  	<?php
-			  	$file2 = file('test.txt');
-			  	debug($file2);
-			  	foreach ($file as $line) {
-		            $info = explode('|', $line);
-		            echo "<tr>";
-			            echo "<td>{$info[0]}</td>";
-			            echo "<td>{$file2[0]}</td>";
-		            echo "</tr>";
+		            $test = str_replace(' ', '', $info[0]);
+		            echo  '<br>'. $info[0];
+		            echo $info[1];
+		            echo "<input type='radio' name='{$test}' value='{$info[1]}'>";
+		            echo $info[2];
+		            echo "<input type='radio' name='{$test}' value='{$info[2]}'>";
+		            echo $info[3];
+		            echo "<input type='radio' name='{$test}' value='{$info[3]}'>";
+        		}
+        		echo  '<br>'. "<input type='submit'>";
+        		if($_SERVER['REQUEST_METHOD'] == 'POST'){
+               		?>
+               		<table class="table">
+					  <thead>
+					    <tr>
+					    	<th scope="col">Вопрос</th>
+					    	<th scope="col">Ваш ответ</th>
+					    </tr>
+					  </thead>
+					  <tbody>
+					    <?php
+					   	$z = 0;
+					    foreach ($_POST as $key => $value) {
+					      	if($key != 'name'){
+					      		$answers[$z] = $value;
+					      		$z++;
+					      	}
+					    }
+					    $z = 0;
+					    foreach ($file_ques as $line) {
+					    	$info = explode('|', $line);
+					      	echo "<tr>";
+					      	echo "<td>{$info[0]}</td>";
+					      	echo "<td>{$answers[$z]}</td>";
+					      	echo "</tr>";
+					      	$z++;
+					    }
+					    ?>
+					  </tbody>
+					</table>
+               		<?php
+            	}
+            	$prav_otv = 0;
+			    if(isset($answers)){
+			    	foreach ($answers as $key => $value) {
+				    	$value = preg_replace('/\s+/', '', $value);
+				    	if(3 == $value){
+				    		$prav_otv++;
+				    	} elseif ('Kohtla-Jarve' == $value) {
+				    		$prav_otv++;
+				    	} elseif (15 == $value){
+				    		$prav_otv++;
+				    	} elseif ('Ида-Вирумаа' == $value){
+				    		$prav_otv++;
+				    	} elseif ('Ревель' == $value){
+				    		$prav_otv++;
+				    	}
+			    	}
+			    	$str_otv = "Кол.-во правельных ответов {$_POST['name']}: " . $prav_otv;
+			    	echo $str_otv;
+			    	$f = fopen("answer.txt", 'w');
+			    	fwrite($f, $str_otv);
+			    	fclose($f);
 
-		        }			  		
-			  	?>			    	
-			  </tbody>
-			</table>
-			<?php
-		}
-		?>
-	
-    </form>
+			    }
+	    	?>
+	    </form>
+
     </body>
 </html>
 
