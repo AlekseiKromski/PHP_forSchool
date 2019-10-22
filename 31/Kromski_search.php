ba62ee48a4dfdfeaf9205233abbcfba847a3ae55
AlekseiKromski <Aleksei.Kromski@ivkhk.ee>
2019-10-22 08:10:49 +0300

    10/22/2019

---
+++ 31/Kromski_search.php
@@ -0,0 +1,46 @@
+<?php
+require_once 'functions.php'; //for debuging array
+require_once 'view_functions.php';
+require_once 'foreachArr.php';
+?>
+<html>
+    <head>
+        <title>Контрольная работа 3 / 2 задание</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel="stylesheet" href="css/style.css" type="text/css"/>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+    	<div class="col-md-6 margin_center">
+    		<a href="Kromski.php">Назад</a>
+    	</div>
+    	<br>
+		<div class="col-md-2 margin_center">
+			<?php
+				if(!empty($_GET)){
+					if($_GET['search'] == 'true'){
+						$search = $_POST['search'];
+						foreach ($city as $key => $value) {
+							if($key == $search){
+								echo "<h5>Результат поиска: </h5>";
+								echo "<h4>{$key}</h4>";
+								echo "<img src='img/{$value['img_name']}' width='150px'>";
+								echo "<br><br>";
+								echo "<p>Количество населения: {$value['count_people']}</p>";
+								echo "<p>Уезд: {$value['maakond']}</p>";
+								echo "<hr>";
+							}else{
+								header('Location: Kromski.php');
+							}
+						}
+					}else{
+						echo "<h5>Запрос не был отпрвлен</h5>";
+					}
+				}else{
+					echo "<h5>Запрос не был отпрвлен</h5>";
+				}
+			?>
+		</div>
+    </body>
+</html>
+
---
+++ 31/view_functions.php
@@ -0,0 +1,206 @@
+<?php
+require_once 'functions.php'; //for debuging array
+
+function viewAllInfo($arr){
+	foreach ($arr as $key => $value) {
+		echo "<h4>{$key}</h4>";
+		echo "<img src='img/{$value['img_name']}' width='150px'>";
+		echo "<br><br>";
+		echo "<p>Количество населения: {$value['count_people']}</p>";
+		echo "<p>Уезд: {$value['maakond']}</p>";
+		echo "<hr>";
+	}
+}
+function viewMenuMaakond(){
+	echo "<a href='Kromski.php'>Все</a> ";
+	echo "<a href='Kromski.php?maakond=Harjumaa'>Harjumaa</a> ";
+	echo "<a href='Kromski.php?maakond=Ida-Virumaa'>Ida-Virumaa</a> ";
+	echo "<a href='Kromski.php?maakond=Tartumaa'>Tartumaa</a>";
+
+}
+
+function viewHarjuMenu($arr){
+	viewMenuMaakond();
+	echo "<br>";
+	foreach ($arr as $key => $value) {
+		if($key == 'Tallinn' or $key == 'Keila' or $key == 'Jõelähtme'){
+			echo "<a href='Kromski.php?maakond={$value['maakond']}&city={$key}'>{$key}</a> ";
+		}
+	}
+}
+function viewTartuMenu($arr){
+	viewMenuMaakond();
+	echo "<br>";
+	foreach ($arr as $key => $value) {
+		if($key == 'Tartu' or $key == 'Elva' or $key == 'Kalaste'){
+			echo "<a href='Kromski.php?maakond={$value['maakond']}&city={$key}'>{$key}</a> ";
+		}
+	}
+}
+function viewIdaMenu($arr){
+	viewMenuMaakond();
+	echo "<br>";
+	foreach ($arr as $key => $value) {
+		if($key == 'Narva' or $key == 'Kohtla-Järve' or $key == 'Jõhvi'){
+			echo "<a href='Kromski.php?maakond={$value['maakond']}&city={$key}'>{$key}</a> ";
+		}
+	}
+}
+function viewCityInfo($arr,$city){
+	if($city == 'Tallinn'){
+		foreach ($arr as $key => $value) {
+			if($key == 'Tallinn'){
+				echo "<h4>{$key}</h4>";
+				echo "<img src='img/{$value['img_name']}' width='150px'>";
+				echo "<br><br>";
+				echo "<p>Количество населения: {$value['count_people']}</p>";
+				echo "<p>Уезд: {$value['maakond']}</p>";
+				echo "<hr>";
+			}
+		}
+	}
+
+	if($city == 'Keila'){
+		foreach ($arr as $key => $value) {
+			if($key == 'Keila'){
+				echo "<h4>{$key}</h4>";
+				echo "<img src='img/{$value['img_name']}' width='150px'>";
+				echo "<br><br>";
+				echo "<p>Количество населения: {$value['count_people']}</p>";
+				echo "<p>Уезд: {$value['maakond']}</p>";
+				echo "<hr>";
+			}
+		}
+	}
+
+	if($city == 'Jõelähtme'){
+		foreach ($arr as $key => $value) {
+			if($key == 'Jõelähtme'){
+				echo "<h4>{$key}</h4>";
+				echo "<img src='img/{$value['img_name']}' width='150px'>";
+				echo "<br><br>";
+				echo "<p>Количество населения: {$value['count_people']}</p>";
+				echo "<p>Уезд: {$value['maakond']}</p>";
+				echo "<hr>";
+			}
+		}
+	}
+
+	if($city == 'Narva'){
+		foreach ($arr as $key => $value) {
+			if($key == 'Narva'){
+				echo "<h4>{$key}</h4>";
+				echo "<img src='img/{$value['img_name']}' width='150px'>";
+				echo "<br><br>";
+				echo "<p>Количество населения: {$value['count_people']}</p>";
+				echo "<p>Уезд: {$value['maakond']}</p>";
+				echo "<hr>";
+			}
+		}
+	}
+	if($city == 'Kohtla-Järve'){
+		foreach ($arr as $key => $value) {
+			if($key == 'Kohtla-Järve'){
+				echo "<h4>{$key}</h4>";
+				echo "<img src='img/{$value['img_name']}' width='150px'>";
+				echo "<br><br>";
+				echo "<p>Количество населения: {$value['count_people']}</p>";
+				echo "<p>Уезд: {$value['maakond']}</p>";
+				echo "<hr>";
+			}
+		}
+	}
+	if($city == 'Jõhvi'){
+		foreach ($arr as $key => $value) {
+			if($key == 'Jõhvi'){
+				echo "<h4>{$key}</h4>";
+				echo "<img src='img/{$value['img_name']}' width='150px'>";
+				echo "<br><br>";
+				echo "<p>Количество населения: {$value['count_people']}</p>";
+				echo "<p>Уезд: {$value['maakond']}</p>";
+				echo "<hr>";
+			}
+		}
+	}
+
+	if($city == 'Tartu'){
+		foreach ($arr as $key => $value) {
+			if($key == 'Tartu'){
+				echo "<h4>{$key}</h4>";
+				echo "<img src='img/{$value['img_name']}' width='150px'>";
+				echo "<br><br>";
+				echo "<p>Количество населения: {$value['count_people']}</p>";
+				echo "<p>Уезд: {$value['maakond']}</p>";
+				echo "<hr>";
+			}
+		}
+	}
+	if($city == 'Elva'){
+		foreach ($arr as $key => $value) {
+			if($key == 'Elva'){
+				echo "<h4>{$key}</h4>";
+				echo "<img src='img/{$value['img_name']}' width='150px'>";
+				echo "<br><br>";
+				echo "<p>Количество населения: {$value['count_people']}</p>";
+				echo "<p>Уезд: {$value['maakond']}</p>";
+				echo "<hr>";
+			}
+		}
+	}
+	if($city == 'Kalaste'){
+		foreach ($arr as $key => $value) {
+			if($key == 'Kalaste'){
+				echo "<h4>{$key}</h4>";
+				echo "<img src='img/{$value['img_name']}' width='150px'>";
+				echo "<br><br>";
+				echo "<p>Количество населения: {$value['count_people']}</p>";
+				echo "<p>Уезд: {$value['maakond']}</p>";
+				echo "<hr>";
+			}
+		}
+	}
+
+}
+function viewMaakondInfo($arr,$maakond){
+	if($maakond == 'Harjumaa'){
+		foreach ($arr as $key => $value) {
+			if($key == 'Tallinn' or $key == 'Keila' or $key == 'Jõelähtme'){
+				echo "<h4>{$key}</h4>";
+				echo "<img src='img/{$value['img_name']}' width='150px'>";
+				echo "<br><br>";
+				echo "<p>Количество населения: {$value['count_people']}</p>";
+				echo "<p>Уезд: {$value['maakond']}</p>";
+				echo "<hr>";
+			}
+		}
+	}elseif($maakond == 'Tartumaa'){
+		foreach ($arr as $key => $value) {
+			if($key == 'Kalaste' or $key == 'Elva' or $key == 'Tartu'){
+				echo "<h4>{$key}</h4>";
+				echo "<img src='img/{$value['img_name']}' width='150px'>";
+				echo "<br><br>";
+				echo "<p>Количество населения: {$value['count_people']}</p>";
+				echo "<p>Уезд: {$value['maakond']}</p>";
+				echo "<hr>";
+			}
+		}
+	}elseif($maakond == 'Ida-Virumaa'){
+		foreach ($arr as $key => $value) {
+			if($key == 'Jõhvi' or $key == 'Narva' or $key == 'Kohtla-Järve'){
+				echo "<h4>{$key}</h4>";
+				echo "<img src='img/{$value['img_name']}' width='150px'>";
+				echo "<br><br>";
+				echo "<p>Количество населения: {$value['count_people']}</p>";
+				echo "<p>Уезд: {$value['maakond']}</p>";
+				echo "<hr>";
+			}
+		}
+	}
+}
+function viewSearchMenu($arr){
+	echo "<br>";
+	echo "<form method='post' action='Kromski_search.php?search=true'>";
+		echo "<input type='search' name='search'>";
+		echo "<input type='submit' value='search'>";
+	echo "</form>";
+}
