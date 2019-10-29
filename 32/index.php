153077e985f4f41abcde95f4ba0ecaca5ea26db1
AlekseiKromski <Aleksei.Kromski@ivkhk.ee>
2019-10-29 09:21:08 +0200

    10/29/2019

--- 32/index.php
+++ 32/index.php
@@ -9,7 +9,13 @@ require_once 'functions.php'; //for debuging array
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
-    	
+    	<?php
+            include_once 'readFromFile.php';
+            ReadFromFile::createLinks();
+            if(isset($_GET['id'])){
+                ReadFromFile::displayInfo($_GET['id']);
+            }
+        ?>
     </body>
 </html>
 
