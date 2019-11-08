594f585a196d4159d884bbdd1612bc13aa213ad8
aleksei.kromski <aleksei.kromski@ivkhk.ee>
2019-11-08 06:53:00 +0200

    mov to archive

--- Foxebiz/js/typeanimation.js
+++
@@ -1,8 +0,0 @@
-
-var typed = new Typed('.typed', {
-         strings: ["fresh.", "fast.","relevant." ],
-         typeSpeed: 80,
-         loop: true,
-         backSpeed: 40,
-         backDelay: 3000
-         });
