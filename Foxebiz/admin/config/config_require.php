1f71467de59c90597936e7b7642c441c6d160f95
aleksei.kromski <aleksei.kromski@ivkhk.ee>
2019-12-09 23:15:05 +0200

    Foxbiz - delete nonMVC project (Make backUp to disk)

--- Foxebiz/admin/config/config_require.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'config_db.php';
-require_once 'global_function.php';
-require_once 'config.php';
-session_start();
-?>
--- Foxebiz/config/config_require.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'config.php';
-require_once 'config_db.php';
-require_once 'functions.php';
